70bfdb3614504cb96eeea14474aa9ae38ba35890
James English <jenglish.ctr@kr.af.mil>
2020-04-24 17:53:02 -0400

    reorganize

--- full_stack_app/frontend/src/index.tsx
+++ full_stack_app/frontend/src/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
-import Button from "@material-ui/core/Button";
-import App from "./App";
+import App from "./components/App";
 
 const rootEl = document.getElementById("root");
 
 render(<App />, rootEl);
 
-//@ts-ignore
-if (module.hot) {
-//@ts-ignore
-    module.hot.accept("./App", () => {
-        const NextApp = require("./App").default;
-        render(<NextApp />, rootEl);
-    });
-}
+
+// todo figure out hot reload? 
+// //@ts-ignore
+// if (module.hot) {
+// //@ts-ignore
+//     module.hot.accept("./App", () => {
+//         const NextApp = require("./App").default;
+//         render(<NextApp />, rootEl);
+//     });
+// }
